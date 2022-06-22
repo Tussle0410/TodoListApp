7ba7288c41ff1c968b45b3e6a425bd2dafab81e8
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-06-23 00:16:50 +0900

    Splash Layout Update

--- app/src/main/java/com/tussle/todolistapp/MainActivity.kt
+++ app/src/main/java/com/tussle/todolistapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tussle.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tussle.todolistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
